045604a852101d5c6181a55a68d053d3536d3f1b
Christine Le <christinele@csu.fullerton.edu>
2019-08-22 17:04:30 -0700

    tracking stats modifications to stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
 var max_matches = 2;
-// var clickIsBlocked=false;
+var attempts = null;
+var games_played = null;
+
 
 function initializeApp() {
 
@@ -18,57 +20,56 @@ function handleCardClick(event) {
   console.log(event);
   $(this).addClass('hidden');
   clickedCard();
+  attempts++;
+  games_played++;
 }
 
 function allCardsAreMatched() {
   if (matches == max_matches)
     $('#modalShadow').removeClass();
 }
-
   $('.modal-footer').on('click', function(){
       $('.modal-footer'.addClass('hidden'))
   })
 
 
 function clickedCard() {
-  // if(clickIsBlocked){
-  //   return;
-  // }
   if (firstCardClicked == null) {
     firstCardClicked = $(event.currentTarget);
     return;
   }
   secondCardClicked = $(event.currentTarget);
 
-
   var urlFirstCard = firstCardClicked.siblings().css('background-image');
   console.log(urlFirstCard);
   var urlSecondCard = secondCardClicked.siblings().css('background-image');
   console.log(urlSecondCard);
 
-  if (urlFirstCard === urlSecondCard) {
-    console.log("Cards Match")
-    $('.lfz-card').addClass('avoid-clicks')
-    matches++;
-    allCardsAreMatched()
-    firstCardClicked = null;
-    secondCardClicked = null;
-    setTimeout(function () {
-      $('.lfz-card').removeClass('avoid-clicks')
-      $(firstCardClicked).removeClass('hidden');
-      $(secondCardClicked).removeClass('hidden');
-    }, 1500);
-  } else if (urlFirstCard !== urlSecondCard) {
-    // clickIsBlocked=true
-    console.log('Cards Dont Match')
-    $('.lfz-card').addClass('avoid-clicks');
-    setTimeout(function () {
-      // clickIsBlocked=false;
-      $(firstCardClicked).removeClass('hidden');
+    if (urlFirstCard === urlSecondCard) {
+      console.log("Cards Match")
+      $('.lfz-card').addClass('avoid-clicks')
+      matches++;
+      allCardsAreMatched()
       firstCardClicked = null;
-      $(secondCardClicked).removeClass('hidden');
       secondCardClicked = null;
-      $('.lfz-card').removeClass('avoid-clicks');
-    }, 1500);
+      setTimeout(function () {
+        $('.lfz-card').removeClass('avoid-clicks')
+        $(firstCardClicked).removeClass('hidden');
+        $(secondCardClicked).removeClass('hidden');
+      }, 1500);
+    } else if (urlFirstCard !== urlSecondCard) {
+      console.log('Cards Dont Match')
+      $('.lfz-card').addClass('avoid-clicks');
+      setTimeout(function () {
+        $(firstCardClicked).removeClass('hidden');
+        firstCardClicked = null;
+        $(secondCardClicked).removeClass('hidden');
+        secondCardClicked = null;
+        $('.lfz-card').removeClass('avoid-clicks');
+      }, 1500);
   }
+}
+
+function calculateAccuracy () {
+
 }
