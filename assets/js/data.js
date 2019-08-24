b33d8828072c27eb11436d813bee0aa95d9f5415
Christine Le <christinele@csu.fullerton.edu>
2019-08-24 12:33:46 -0700

    Final version1 - trying to do different stages

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,15 +4,27 @@ var rounds = [
   {
     background: "../images/pokemonimages/viridian-forest-1.jpg",
     pokemon: [
-      { pokemonType: 'caterpie', front: 'pokemon-butterfree'},
-      { pokemonType: 'caterpie', front: 'pokemon-caterpie' }
-    ],
+      { pokemonType: 'caterpie', front: 'caterpie' },
+      { pokemonType: 'Lickitung', front: 'Lickitung' },
+      { pokemonType: 'oddish', front: 'oddish' },
+      { pokemonType: 'jigglypuff', front: 'jigglypuff' },
+      { pokemonType: 'farfetch', front: 'farfetch' },
+      { pokemonType: 'exeggcute', front: 'exeggutor' },
+      { pokemonType: 'snorlax', front: 'snorlax' },
+      { pokemonType: 'tangela', front: 'tangela' },
+      { pokemonType: 'scyther', front: 'scyther' },
   },
   {
-    background: "../images/pokemonimages/viridian-swamp-1.jpg",
-    pokemon: [
-      'pokemon-uzair',
-      'pokemon-pikabutt'
-    ]
-  },
+  //   background: "../images/pokemonimages/mtMoon.png",
+  //   pokemon: [
+
+  //     { pokemonType: 'caterpie', front: 'butterfree' }
+
+  //     { pokemonType: 'oddish', front: 'gloom' }
+
+  //     { pokemonType: 'jigglypuff', front: 'wigglytuff' }
+  //     { pokemonType: 'exeggcute', front: 'exeggcute' }
+
+  //   ]
+  // },
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,61 @@ var matches = null;
 var max_matches = 9;
 var attempts = 0;
 var games_played = 0;
-var audio = new Audio("SFX_TURN_ON_PC.wav");
-
-// var currentRound = 0;
-
-
-
+// var audio = new Audio("SFX_TURN_ON_PC.wav");
+var currentRound = 0;
+var rounds = [
+  {
+    background: "../images/pokemonimages/viridian-forest-1.jpg",
+    pokemon: [
+      { pokemonType: 'caterpie', front: 'caterpie' },
+      { pokemonType: 'caterpie', front: 'caterpie' },
+      { pokemonType: 'Lickitung', front: 'Lickitung' },
+      { pokemonType: 'Lickitung', front: 'Lickitung' },
+      { pokemonType: 'oddish', front: 'oddish' },
+      { pokemonType: 'oddish', front: 'oddish' },
+      { pokemonType: 'jigglypuff', front: 'jigglypuff' },
+      { pokemonType: 'jigglypuff', front: 'jigglypuff' },
+      { pokemonType: 'farfetch', front: 'farfetch' },
+      { pokemonType: 'farfetch', front: 'farfetch' },
+      { pokemonType: 'exeggcutor', front: 'exeggcutor' },
+      { pokemonType: 'exeggcutor', front: 'exeggcutor' },
+      { pokemonType: 'snorlax', front: 'snorlax' },
+      { pokemonType: 'snorlax', front: 'snorlax' },
+      { pokemonType: 'tangela', front: 'tangela' },
+      { pokemonType: 'tangela', front: 'tangela' },
+      { pokemonType: 'scyther', front: 'scyther' },
+      { pokemonType: 'scyther', front: 'scyther' },
+    ]
+  },
+  {
+    background: "../images/pokemonimages/mtMoon.png",
+    pokemon: [
+      { pokemonType: 'cubone', front: 'cubone' },
+      { pokemonType: 'cubone', front: 'marowak' },
+      { pokemonType: 'slowpoke', front: 'slowpoke' },
+      { pokemonType: 'slowpoke', front: 'slowbrow' },
+      { pokemonType: 'clefairy', front: 'clefairy' },
+      { pokemonType: 'clefairy', front: 'clefable' },
+      { pokemonType: 'meowth', front: 'meowth' },
+      { pokemonType: 'meowth', front: 'persian' },
+      { pokemonType: 'kabuto', front: 'kabuto' },
+      { pokemonType: 'kabuto', front: 'kabutops' },
+      { pokemonType: 'gastly', front: 'gastly' },
+      { pokemonType: 'gastly', front: 'gengar' },
+      { pokemonType: 'omanyte', front: 'omanyte' },
+      { pokemonType: 'omanyte', front: 'omastar' },
+      { pokemonType: 'abra', front: 'abra' },
+      { pokemonType: 'abra', front: 'Alakazam' },
+      // { pokemonType: 'clefairy', front: 'clefairy' },
+      // { pokemonType: 'clefairy', front: 'clefable' },
+    ]
+  },
+]
 
 function initializeApp() {
-  audio.play();
-  // loadCurrentRound();
+  // audio.play();
+  loadCurrentRound();
+  shuffleDeck();
   $('.pokeball').on('click', handleCardClick);
   $('.modalClose').click(function() {
       $('#modalShadow').toggleClass('hidden');
@@ -24,29 +69,25 @@ function initializeApp() {
       displayStats();
   });
 }
-// function loadCurrentRound() {
-//   var background = rounds[currentRound].background;
-//   var cards = rounds[currentRound].pokemon;
-//   $("#pokemonArena").empty();
-//   for( var pokeI = 0; pokeI < cards.length; pokeI++){
-//     var currentCard = cards[pokeI];
-//     addCardToGameArea( currentCard.front, currentCard.pokemonType);
-//   }
-// }
-/*
-        <div class='card'>
-          <div class='pokeball' data-pokemon="caterpie"></div>
-          <div class='pokemon-butterfree'></div>
-        </div>
-*/
-// function addCardToGameArea( pokemonClass, pokemonType ){
-//   var card = $("<div>").addClass('card');
-//   var pokeball = $("<div>").addClass('pokeball').attr('data-pokemon', pokemonType);
-//   var pokemon = $("<div>").addClass( pokemonClass );
 
-//   card.append( pokeball, pokemon);
-//   $("#pokemonArena").append(card);
-// }
+function loadCurrentRound() {
+  var background = rounds[currentRound].background;
+  var cards = rounds[currentRound].pokemon;
+  $('#pokemonArena').empty();
+    for( var pokeI = 0; pokeI < cards.length; pokeI++){
+    var currentCard = cards[pokeI];
+    addCardToGameArea( currentCard.front, currentCard.pokemonType);
+    }
+}
+
+function addCardToGameArea( pokemonClass, pokemonType ){
+  var card = $("<div>").addClass('card');
+  var pokeball = $("<div>").addClass('pokeball').attr('data-pokemon', pokemonType);
+  var pokemon = $("<div>").addClass( pokemonClass);
+
+  card.append( pokeball, pokemon);
+  $("#pokemonArena").append(card);
+}
 
 function handleCardClick(event) {
   console.log(event);
@@ -58,9 +99,9 @@ function allCardsAreMatched() {
   if (matches == max_matches)
     $('#modalShadow').removeClass();
 }
-  $('.modal-footer').on('click', function(){
-      $('.modal-footer'.addClass('hidden'))
-  })
+  // $('.modal-footer').on('click', function(){
+  // $('.modal-footer'.addClass('hidden'))
+  // })
 
 
 function clickedCard() {
@@ -70,11 +111,11 @@ function clickedCard() {
   }
   secondCardClicked = $(event.currentTarget);
 
-  // var urlFirstCard = firstCardClicked.attr('data-pokemon');
-var urlFirstCard = firstCardClicked.siblings().css('background-image');
+  var urlFirstCard = firstCardClicked.attr('data-pokemon');
+// var urlFirstCard = firstCardClicked.siblings().css('background-image');
   console.log(urlFirstCard);
-  // var urlSecondCard = secondCardClicked.attr('data-pokemon');
-var urlSecondCard = secondCardClicked.siblings().css('background-image');
+  var urlSecondCard = secondCardClicked.attr('data-pokemon');
+// var urlSecondCard = secondCardClicked.siblings().css('background-image');
   console.log(urlSecondCard);
 
     if (urlFirstCard === urlSecondCard) {
@@ -108,6 +149,7 @@ var urlSecondCard = secondCardClicked.siblings().css('background-image');
     games_played++;
     displayStats();
     resetStats();
+    shuffleDeck();
   }
 }
 
@@ -126,3 +168,25 @@ function resetStats() {
   matches = null;
   attempts = 0;
 }
+
+// function shuffleDeck(pokemon) {
+//   var randomNumber;
+//   var arrayLength = pokemon.length - 1
+//   var spotHolder;
+//   for (var i = 0; i < arrayLength; i++) {
+//     randomNumber = Math.floor(Math.random() * (pokemon.length));
+//     spotHolder = pokemon[i];
+//     pokemon[i] = pokemon[randomNumber];
+//     pokemon[randomNumber] = spotHolder;
+//   }
+//   return pokemon;
+// }
+
+function shuffleDeck() {
+  for (var i = 0, i < rounds.pokemon[pokemonType], i++) {
+    randomNumber = Math.floor(Math.random() * rounds.pokemon[pokemonType]);
+  }
+  return rounds.pokemon[pokemonType];
+}
+
+  // $('.modalClose').on('click', loadCurrentRound());
