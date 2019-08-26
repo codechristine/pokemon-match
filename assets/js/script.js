114f22b88f6c05c4f7dc74ec9758d25b22d9c3ae
Christine Le <christinele@csu.fullerton.edu>
2019-08-26 15:52:04 -0700

    revised max_matches value

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(initializeApp);
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
-var max_matches = 2;
+var max_matches = 9;
 var attempts = 0;
 var games_played = 0;
 var currentRound = -1;
